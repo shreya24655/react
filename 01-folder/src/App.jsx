dec99be2a006e656c9db5726e35d54db1cca7cf2
Shreya Singh <shreya.24655@knit.ac.in>
2026-08-15 18:38:55 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id='parent1'>
+      <h1 id='child1'>app</h1>
+      <h2 id='child2'>Hello</h2>
+    </div>
+    <div id='parent2'>yupp</div>
+  </>
+  )
+}
+
+export default App
